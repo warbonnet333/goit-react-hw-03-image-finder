920466f18fde9a07e0ac23f0e02be91bc0db7bb9
warbonnet333 <warbonnet333@gmail.com>
2020-02-25 02:37:46 +0200

    Updates

---
+++ precache-manifest.cd76de71d0cfd64be3a5733042b1a058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028dda8debc78961bf07cb70c0c9150f",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "810163ddb3d4a1e44ef8",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.d0985932.chunk.css"
+  },
+  {
+    "revision": "02f4570ec5deab314aed",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.9fb94252.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.9fb94252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "810163ddb3d4a1e44ef8",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.9f5869c1.chunk.js"
+  },
+  {
+    "revision": "10e27ea3e06308bdf8d4",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.1b4effe2.js"
+  }
+]);
